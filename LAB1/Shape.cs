52a611dd17a593349ab2b448155459d8e7c61654
Nikita Voino <ankapkekw@gmail.com>
2025-03-06 12:21:50 +0300

    Add abstract class Shape

---
+++ LAB1/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    abstract class Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Symbol { get; set; }
+        public Shape(int x, int y, char symbol)
+        {
+            X = x;
+            Y = y;
+            Symbol = symbol;
+        }
+        public abstract void Draw(char[,] canvas);
+    }
+}
