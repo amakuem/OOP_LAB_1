7cd1fcd51ed48b7b423a666b295183d9c25c8795
Nikita Voino <ankapkekw@gmail.com>
2025-05-07 19:42:01 +0300

    Add class diagram

--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49f0993061a57d3a142454723e2200ed5430c468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b24e1960e5b29bf3aedbdbd4537f2d09d1f631e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
